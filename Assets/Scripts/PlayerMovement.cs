6196c0a5392f60b82020feacc0e0c8776e5f7e95
lwhite14 <55700734+lwhite14@users.noreply.github.com>
2021-06-22 15:18:09 +0100

    feat: improved jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,17 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rigid;
     public float speed;
-    public float jumpForce;
     [HideInInspector]
     public bool isLanded = true;
     float xMove;
+
+    [Header("Jumping Variables")]
+    public float jumpForce;
+    public float groundedRememberTime = 0.2f;
+    float groundedRemember = 0f;
+    public float jumpPressedTimer = 0.2f;
+    float jumpPressedRemeber = 0f;
+    public float cutJumpHeight = 0.5f;
 
 
     void Update()
@@ -24,12 +31,35 @@ public class PlayerMovement : MonoBehaviour
         transform.position = new Vector3(transform.position.x + (xMove * Time.deltaTime * speed), transform.position.y, 0);
     }
 
-    void Jump() 
+    void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isLanded) 
+        groundedRemember -= Time.deltaTime;
+        jumpPressedRemeber -= Time.deltaTime;
+
+        if (isLanded)
         {
-            rigid.AddForce(transform.up * jumpForce);
+            groundedRemember = groundedRememberTime;
+        }    
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpPressedRemeber = jumpPressedTimer;
         }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if (!isLanded)
+            {
+                rigid.velocity = new Vector2(rigid.velocity.x, rigid.velocity.y * cutJumpHeight);
+            }
+        }
+
+        if ((jumpPressedRemeber > 0) && (groundedRemember > 0) && isLanded)
+        {
+            jumpPressedRemeber = 0f;
+            groundedRemember = 0f;
+            rigid.AddForce(transform.up * jumpForce);
+
+        }
+
     }
 
     public void SetIsLanded(bool newIsLanded) 
