231e49d693ea3146695f290a33407970459d51ab
lwhite14 <55700734+lwhite14@users.noreply.github.com>
2021-06-25 18:39:11 +0100

    feat; basic camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing = 5.0f;
+    bool stopFollowing = false;
+
+    void Update()
+    {
+        if ((target != null) && (!stopFollowing))
+        {
+            Vector3 newPos = new Vector3(target.position.x, target.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, newPos, (smoothing * 0.001f));
+        }
+
+        if (transform.position.y <= 0)
+        {
+            stopFollowing = true;
+            transform.position = new Vector3(transform.position.x, 0, -10);
+        }
+        else
+        {
+            stopFollowing = false;
+        }
+    }
+
+    public void SetTarget(Transform newTarget) 
+    {
+        target = newTarget;
+    }
+
+}
